f0786d8d8d90afff6076e0c948a7c274b37e1aad
Noel Vock <noel1uk@gmail.com>
2018-05-14 12:10:48 +0100

    test: ensure anagram for given word can be found if it is in `knownWords`

--- jasmine/spec/AnagramSpec.js
+++ jasmine/spec/AnagramSpec.js
@@ -8,18 +8,42 @@ describe('Anagram', function() {
     });
   });
 
+
+  // This test now seems unnecessary as the pushNewWord function is used in the "when a word has 
+  // been added to `knownWords' describe block"
+  // 
   describe('.pushNewWord', function() {
 
     beforeEach(function() {
-      anagram.pushNewWord('word');
+      anagram.pushNewWord('door');
     });
 
     afterEach(function() {
       anagram.knownWords = [];
     });
-
+    
     it('adds new words to `knownWords`', function() {
-      expect(anagram.knownWords).toContain('word');
+      expect(anagram.knownWords).toContain('door');
     });
   });
+
+  describe('when a word has been added to `knownWords', function() {
+    
+    beforeEach(function() {
+      anagram.pushNewWord('word');
+    });
+  
+    afterEach(function() {
+      anagram.knownWords = [];
+    });
+
+    describe('.find', function() {
+  
+      it('.find', function() {
+        expect(anagram.find('drow')).toEqual('word');
+      });
+    });
+  });
+
+
 });
