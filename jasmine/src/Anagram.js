896a182c61a9217394a1c9aed5977c76c37bd22a
Noel Vock <noel1uk@gmail.com>
2018-05-14 14:17:50 +0100

    feat: returns an array of matched word

--- jasmine/src/Anagram.js
+++ jasmine/src/Anagram.js
@@ -1,9 +1,39 @@
 class Anagram {
   constructor() {
     this.knownWords = [];
+    this.matchedWords = []
   }
 
   pushNewWord(word) {
     this.knownWords.push(word);
+  }
+
+  find(word) {
+    this.knownWords.forEach(function(knownWord) {
+      if (this.wordLengthMatches(word, knownWord)) {
+        for ( let i = 0, tempWord = ''; i < word.length; i++ ) {
+          if ( this.letterIsInWord(word[i], knownWord) ) {
+            if ( i + 1 === word.length ) {
+              this.matchedWords.push(knownWord)
+            }
+          } else {
+            break;
+          }
+        }
+      }
+    }, this);
+    return(this.matchedWords);
+  }
+
+  wordLengthMatches(word, knownWord) {
+    if ( word.length === knownWord.length ) {
+      return true;
+    }
+  }
+
+  letterIsInWord(letter, knownWord) {
+    if ( knownWord.indexOf(letter) > -1 ) {
+      return true;
+    }
   }
 }
