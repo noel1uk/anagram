9fc8189f1bc5536b175ac08c0a3d03d7a9022d68
Noel Vock <noel1uk@gmail.com>
2018-05-14 11:19:50 +0100

    test: ensure `pushNewWord` adds a new word to `knownWords`

--- jasmine/spec/AnagramSpec.js
+++ jasmine/spec/AnagramSpec.js
@@ -1,7 +1,16 @@
 describe('Anagram', function() {
 
   let anagram = new Anagram()
+
+
   it('initializes with an empty array', function() {
     expect(anagram.knownWords).toEqual([]);
+  });
+
+  describe('.pushNewWord', function() {
+    it('adds new words to `knownWords`', function() {
+      anagram.pushNewWord('word');
+      expect(anagram.knownWords).toContain('word');
+    });
   });
 });
